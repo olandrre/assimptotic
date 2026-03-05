66979ccdd56be981dfbb54bd147137b631c05959
olandrre <olandrre@MacBook-Air-olandrre.local>
2026-03-05 18:44:28 +0300

    правки

--- py/graphics.py
+++ py/graphics.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('results/binary.csv', index_col=0)
-data2 = pd.read_csv('results/linear.csv', index_col=0)
+data = pd.read_csv('results/strategy_A.csv', index_col=0)
+data2 = pd.read_csv('results/strategy_B.csv', index_col=0)
+data3 = pd.read_csv('results/strategy_C.csv', index_col=0)
 
 plt.figure(figsize=(10, 6))
 plt.errorbar(data['X'], data['Y'], yerr=0, xerr=0, fmt='.', capsize=3, color='green')
 plt.errorbar(data2['X'], data2['Y'], yerr=0, xerr=0, fmt='.', capsize=3, color='blue')
+plt.errorbar(data3['X'], data3['Y'], yerr=0, xerr=0, fmt='.', capsize=3, color='red')
 
 plt.xscale('log') 
 plt.yscale('log')  
